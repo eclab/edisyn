87b1087cf2a8cb2915150bdbb9b350ada049d5db
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2017-12-12 21:24:48 -0500

    Kawai K1; MIDI test additions

--- edisyn/gui/SynthPanel.java
+++ edisyn/gui/SynthPanel.java
@@ -30,6 +30,38 @@ public class SynthPanel extends JPanel implements Gatherable
 
     public Insets getInsets() { return Style.SYNTH_PANEL_INSETS; }
 
+// I thought maybe I'd make little dots which lit up when MIDI came in, but
+// have changed my mind.
+/*
+	public void paint(Graphics graphics)
+		{
+		super.paint(graphics);
+		
+		// At this point all children have been painted.  So we're going to query
+		// the synth to determine whether to draw some MIDI dots.  However we don't
+		// have the synth proper.  We work our way up until we reach it.
+		Component component = this;
+		while((component != null) && !(component instanceof Synth))
+			{
+			component = component.getParent();
+			}
+		if (component == null) return;
+		Synth synth = (Synth)component;
+
+		Graphics2D g = (Graphics2D)graphics;
+		if (true); // synth.incomingControllerMIDI())
+			{
+			g.setColor(Color.red);
+			g.fillOval(getWidth() - 6 - 2, 2, 6, 6);
+			}
+		//if (synth.incomingSynthMIDI())
+			{
+			g.setColor(Color.white);
+			g.fillOval(getWidth() - 4 * 2 - 2 * 2, 2, 4, 4);
+			}		
+		}
+*/
+
     public void gatherAllComponents(java.util.ArrayList list)
         {
         Component[] c = getComponents();

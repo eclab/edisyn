729290fcc2f447dea66da81059cb181415c94948
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2018-10-27 19:50:31 -0400

    yo

---
+++ edisyn/synth/emumorpheus/DumpFilters.java
@@ -0,0 +1,17 @@
+public class DumpFilters
+    {
+    public static void main(String[] args) throws java.io.IOException
+        {
+        // FILTERS
+        for(int i = 0; i < 7; i++) System.in.read();
+        while(true)
+            {
+            System.out.print((int)(System.in.read() + System.in.read() * 128));
+            System.out.print("\t");
+            for(int i = 0; i < 12; i++)
+                System.out.print(Character.toString((char)(System.in.read())));
+            //System.out.print("\t" + (int)(System.in.read() + System.in.read() * 128));
+            System.out.println();
+            }
+        }
+    }

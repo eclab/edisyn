a675e74c079543c6e0e3664f3c6888094e51f1ea
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2021-07-25 18:17:46 -0400

    prep for lib

--- edisyn/Patch.java
+++ edisyn/Patch.java
@@ -7,16 +7,21 @@ package edisyn;
 import edisyn.util.*;
 
 /**
-   Sysex data for a single patch
+   Sysex data for an atomic collection of sysex messages sufficient 
+   to define a patch or a patch bank (not bulk patch sysex).
 */
 
 public class Patch
     {
+    // Is this a bank sysex message?
     public boolean bank;
+    // Sysex messages
     public byte[][] sysex;
+    // Which kind of synth editor is designed for this Patch?
     public int synth;
+    // Patch name
     public String name = null;
-    int index = 0;
+    // Patch location
     public String location = null;
     
     public Patch(int synth, byte[][] sysex, boolean bank)
@@ -27,8 +32,13 @@ public class Patch
         } 
     
     public String toString()
+    	{
+    	return name;
+    	}
+    /*
+    public String toString()
         {
-        String s = "Patch[" + (location == null ? "" : location) + " " + name + ", " + synth + ", " + bank + ", " + index + ", " + sysex.length + "\n";
+        String s = "Patch[" + (location == null ? "" : location) + " " + name + ", " + synth + ", " + bank + ", " + sysex.length + "\n";
         for(int i = 0; i < sysex.length; i++)
             {
             s = s + ("" + i + " (" + sysex[i].length + ") ->");
@@ -40,6 +50,7 @@ public class Patch
             }
         return s + "\n] ";
         }
+    */
         
     }
         

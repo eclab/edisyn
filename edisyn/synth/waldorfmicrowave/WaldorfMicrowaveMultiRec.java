59aaa57adbb24f04a2d6abe38136074b0655138f
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2022-03-18 15:38:51 -0400

    bank recognition

--- edisyn/synth/waldorfmicrowave/WaldorfMicrowaveMultiRec.java
+++ edisyn/synth/waldorfmicrowave/WaldorfMicrowaveMultiRec.java
@@ -1,5 +1,5 @@
 /**
-   Copyright 2021 by Sean Luke
+   Copyright 2022 by Sean Luke
    Licensed under the Apache License version 2.0
 */
 
@@ -10,21 +10,27 @@ public class WaldorfMicrowaveMultiRec extends Recognize
     {
     public static boolean recognize(byte[] data)
         {
-        boolean v = (
+        return (
             data.length == EXPECTED_SYSEX_LENGTH &&
             data[0] == (byte)0xF0 &&
             data[1] == (byte)0x3E &&
             data[2] == (byte)0x00 &&
             data[4] == (byte)0x43) ||           // ARPD Dump
-            (
-            data.length == 14471 &&
+            
+            recognizeBank(data);
+        }
+        
+    
+    public static boolean recognizeBank(byte[] data)
+        {
+        return  (
+            data.length == EXPECTED_BULK_LENGTH &&
             data[0] == (byte)0xF0 &&
             data[1] == (byte)0x3E &&
             data[2] == (byte)0x00 &&
             data[4] == (byte)0x51);             // APBD Bank Dump
-        return v;
-        }
-        
-    
+        } 
+
     public static final int EXPECTED_SYSEX_LENGTH = 233;
+    public static final int EXPECTED_BULK_LENGTH = 14471;
     }
--- edisyn/synth/waldorfmicrowave/WaldorfMicrowaveRec.java
+++ edisyn/synth/waldorfmicrowave/WaldorfMicrowaveRec.java
@@ -1,5 +1,5 @@
 /**
-   Copyright 2021 by Sean Luke
+   Copyright 2022 by Sean Luke
    Licensed under the Apache License version 2.0
 */
 
@@ -9,10 +9,11 @@ import edisyn.*;
 public class WaldorfMicrowaveRec extends Recognize
     {
     public static final int EXPECTED_SYSEX_LENGTH = 187;        
+    public static final int EXPECTED_BULK_LENGTH = 11527;        
 
     public static boolean recognize(byte[] data)
         {
-        boolean v = (
+        return (
             data.length == EXPECTED_SYSEX_LENGTH &&
             data[0] == (byte)0xF0 &&
             data[1] == (byte)0x3E &&
@@ -24,12 +25,17 @@ public class WaldorfMicrowaveRec extends Recognize
             data[1] == (byte)0x3E &&
             data[2] == (byte)0x00 &&
             data[4] == (byte)0x4B) ||           // ABPD Dump, not likely to happen
-            (
+            
+            recognizeBank(data);
+        }
+
+    public static boolean recognizeBank(byte[] data)
+        {
+        return  (
             data.length == 11527 &&
             data[0] == (byte)0xF0 &&
             data[1] == (byte)0x3E &&
             data[2] == (byte)0x00 &&
             data[4] == (byte)0x50);             // BPBD Bank Dump
-        return v;
-        }
+        } 
     }

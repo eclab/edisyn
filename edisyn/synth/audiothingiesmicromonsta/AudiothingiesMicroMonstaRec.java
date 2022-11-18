f7cfe90a923ec1841a8f6a064609e7db3e8f470b
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2022-11-18 17:15:17 -0500

    indented

--- edisyn/synth/audiothingiesmicromonsta/AudiothingiesMicroMonstaRec.java
+++ edisyn/synth/audiothingiesmicromonsta/AudiothingiesMicroMonstaRec.java
@@ -11,13 +11,13 @@ public class AudiothingiesMicroMonstaRec extends Recognize
     public static boolean recognize(byte[] data)
         {
         boolean v = (data.length == 191 &&
-	            data[0] == (byte)0xF0 &&
-	            data[1] == (byte)0x00 &&
-    	        data[2] == (byte)0x21 &&
-        	    data[3] == (byte)0x22 &&
-        	    data[4] == (byte)0x4D &&
-        	    data[5] == (byte)0x4D &&
-        	    data[6] == (byte)0x03);
+            data[0] == (byte)0xF0 &&
+            data[1] == (byte)0x00 &&
+            data[2] == (byte)0x21 &&
+            data[3] == (byte)0x22 &&
+            data[4] == (byte)0x4D &&
+            data[5] == (byte)0x4D &&
+            data[6] == (byte)0x03);
         return v;
         }
     }

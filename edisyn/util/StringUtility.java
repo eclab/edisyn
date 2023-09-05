9daa4f7e32a892795948bf5e2849aad3dd2ca152
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2023-09-05 22:02:52 +0200

    html stripping

--- edisyn/util/StringUtility.java
+++ edisyn/util/StringUtility.java
@@ -5,6 +5,10 @@
 
 package edisyn.util;
 import java.util.*;
+import javax.swing.text.*;
+import javax.swing.text.html.*;
+import javax.swing.text.html.parser.*;
+import java.io.*;
 
 public class StringUtility
     {
@@ -383,5 +387,57 @@ public class StringUtility
         {
         return String.format("%08X", val);
         }
+        
+    /** Tests for HTML in text. */
+    public static boolean testHTML(String val)
+    	{
+    	return val.trim().toLowerCase().startsWith("<html>");
+    	}
+    	
+    /** Strips HTML from text. */
+    public static String removeHTML(String val)
+    	{
+    	try
+    		{
+    	StringBuffer s = new StringBuffer();
+    	new ParserDelegator().parse(new StringReader(val), 
+    		new HTMLEditorKit.ParserCallback()
+    			{
+    			public void handleText(char[] text, int pos) { s.append(text); }
+    			public void handleSimpleTag(HTML.Tag t, MutableAttributeSet a, int pos) 
+    				{
+    				if (t.equals(HTML.Tag.BR) || 
+    					t.equals(HTML.Tag.CENTER) || 
+    					t.equals(HTML.Tag.LI) || 
+    					t.equals(HTML.Tag.DD) || 
+    					t.equals(HTML.Tag.DT) || 
+    					t.equals(HTML.Tag.H1) || 
+    					t.equals(HTML.Tag.H2) || 
+    					t.equals(HTML.Tag.H3) || 
+    					t.equals(HTML.Tag.H4) || 
+    					t.equals(HTML.Tag.H5) || 
+    					t.equals(HTML.Tag.H6) || 
+    					t.equals(HTML.Tag.P) || 
+    					t.equals(HTML.Tag.TH) || 
+    					t.equals(HTML.Tag.TD)) s.append(" ");
+    				}
+    			public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) 
+    				{
+    				if (t.equals(HTML.Tag.CENTER) || 
+    					t.equals(HTML.Tag.H1) || 
+    					t.equals(HTML.Tag.H2) || 
+    					t.equals(HTML.Tag.H3) || 
+    					t.equals(HTML.Tag.H4) || 
+    					t.equals(HTML.Tag.H5) || 
+    					t.equals(HTML.Tag.H6) || 
+    					t.equals(HTML.Tag.P) || 
+    					t.equals(HTML.Tag.TH) || 
+    					t.equals(HTML.Tag.TD)) s.append(" ");
+    				}
+    			}, Boolean.TRUE);
+    	return s.toString();
+    	}
+    	catch (IOException ex) { return val; }		// should never happenx
+    	}
     }
 

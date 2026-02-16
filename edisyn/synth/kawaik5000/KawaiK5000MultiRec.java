18537f0aeaec0d1a5523dc66c27c0ece51b63d8c
Sean Luke <sean@cs.gmu.edu>
2026-02-15 23:31:53 -0500

    KawaiK5000.html

---
+++ edisyn/synth/kawaik5000/KawaiK5000MultiRec.java
@@ -0,0 +1,22 @@
+/**
+   Copyright 2026 by Sean Luke
+   Licensed under the Apache License version 2.0
+*/
+
+package edisyn.synth.kawaik5000;
+import edisyn.*;
+
+public class KawaiK5000MultiRec extends Recognize
+    {
+    public static boolean recognize(byte[] data)
+        {
+        return (data.length == 9 + 54 + 4 * 12 + 1 &&
+           	    (data[0] & 0xFF) == 0xF0 &&
+                 data[1] == 0x40 &&
+                 data[3] == 0x20 &&
+                 data[4] == 0x00 &&
+                 data[5] == 0x0A &&
+                 data[6] == 0x20 &&
+                (data[data.length - 1] & 0xFF) == 0xF7);
+        }
+    }
